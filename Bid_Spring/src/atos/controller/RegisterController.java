ce3d92382985cb54222e944fa11f515025364e63
Unknown <psyyw6@nottingham.ac.uk>
2017-12-10 19:33:16 +0000

    Registration Controller for Login System

---
+++ Bid_Spring/src/atos/controller/RegisterController.java
@@ -0,0 +1,42 @@
+package atos.controller;
+
+import atos.dao.UserDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class RegisterController {
+
+    @Resource
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    @RequestMapping(value = "/reg.do",method = GET)
+    public String RegUser(HttpServletRequest request, ModelMap model) {
+        String name = request.getParameter("username");
+        String pwd = request.getParameter("password");
+        String cf_pwd = request.getParameter("cf_password");
+        String email = request.getParameter("email");
+        String department = request.getParameter("department");
+
+        if(!pwd.equals(cf_pwd)) {
+            return "register";
+        }
+        userDao.RegUsers(name,pwd,email,department);
+        return "login";
+    }
+
+}
