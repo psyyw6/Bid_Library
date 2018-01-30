9bfe69cad432251c8947e4caa45d75fc2b744628
Unknown <psyyw6@nottingham.ac.uk>
2018-01-30 23:23:28 +0000

    add the controller class file

---
+++ Bid_Spring/src/atos/controller/AdminController.java
@@ -0,0 +1,23 @@
+package atos.controller;
+
+import atos.dao.UserDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+public class AdminController {
+    @Resource
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+}
