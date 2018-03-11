b4642eb91628503ef0cd3519ebedf42bf4422f62
Shanshan LI <405321219@qq.com>
2018-03-11 22:27:54 +0000

    Add staff exception handler class

---
+++ src/atos/exceptions/StaffException.java
@@ -0,0 +1,36 @@
+package atos.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class StaffException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private String errCode;
+    private String errMsg;
+
+    public StaffException(String errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public String getErrCode() {
+        return errCode;
+    }
+
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+
+
+
+}
