a0e909ee4890fbab0b33ae8da6609a2aab15edd5
Shanshan LI <405321219@qq.com>
2018-03-18 01:48:47 +0000

    Add template class

---
+++ src/atos/admain/InternalTemplate.java
@@ -0,0 +1,29 @@
+package atos.admain;
+
+import java.util.HashMap;
+
+public class InternalTemplate {
+
+    HashMap<String, Object> internal;
+
+    InternalTemplate(){
+        internal = new HashMap<String, Object>();
+        internal.put("ImmediateandSustainedReductioninOperatingExpense", "");
+        internal.put("ModularandScalableSolution", "");
+        internal.put("StandardisationandContinuity", "");
+        internal.put("SimplicityandInnovation", "");
+        internal.put("RequirementsChecklist", "");
+        internal.put("InScope", "");
+        internal.put("OutOfScope", "");
+        internal.put("ManagedService", "");
+        internal.put("Software Licences", "");
+        internal.put("TechnicalComponentsoftheSolution", "");
+        internal.put("EnvironmentalConsiderations", "");
+        internal.put("AssistiveTechnology", "");
+        internal.put("OutlineDeliveryApproach", "");
+        internal.put("Risks", "");
+        internal.put("Assumptions", "");
+        internal.put("Issues", "");
+        internal.put("Dependencies", "");
+    }
+}
